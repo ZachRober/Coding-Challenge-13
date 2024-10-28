1cea2565668351d9b82c5f8b509fdc6a68e9e5b6
ZachRober <nolethedgehogdie@gmail.com>
2024-10-28 15:35:56 -0400

    Format product information dynamically

---
+++ main.js
@@ -0,0 +1,29 @@
+const myContainer = document.getElementById('container')
+const paragraphContainer = document.getElementById('paragraph-container')
+fetch("https://www.course-api.com/javascript-store-products")
+    .then(response=>{
+        if(!response.ok){
+            throw new Error("Bad URL");}
+        return response.json();
+    })
+    .then(data=>{
+        for (let i = 0; i < data.length; i++) {
+            let companyName = data[i].fields.company;
+            let companyImage = data[i].fields.image;
+            let productName = data[i].fields.name;
+            let productPrice = data[i].fields.price;
+            let productNew = document.createElement("p");
+            let imageNew = document.createElement("img");
+            imageNew.src=companyImage[0].url
+            productNew.textContent = `Sold by ${companyName}, ${productName}, Price: $${productPrice}`
+            myContainer.append(productNew,imageNew);
+            productNew.style.color = "Orange"
+            productNew.style.fontSize = "30px"
+
+
+            
+            
+        }
+    })
+    .catch(error=>console.error(error));
+    document.body.style.backgroundColor = "lightblue"
